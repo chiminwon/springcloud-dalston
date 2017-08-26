356069c58dd4cdbb876f01becd92dd20df1955a9
chiminwon <chiminwon@gmail.com>
2017-08-26 09:28:46 +0800

    rename the main class

--- src/main/java/com/ming/springcloud/DalstonConfigClientGitApp.java
+++ src/main/java/com/ming/springcloud/DalstonConfigClientGitApp.java
@@ -2,12 +2,14 @@ package com.ming.springcloud;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class DalstonConfigClientApp {
+public class DalstonConfigClientGitApp {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(DalstonConfigClientApp.class).web(true).run(args);
+		new SpringApplicationBuilder(DalstonConfigClientGitApp.class).web(true).run(args);
 	}
 
 }
