23e3962a2ad28fc2c6a2f9dcc1a4fdb5493189fb
chiminwon <chiminwon@gmail.com>
2017-08-27 18:59:29 +0800

    change the annotation

--- src/main/java/com/ming/springcloud/DalstonHystrixDashboardApp.java
+++ src/main/java/com/ming/springcloud/DalstonHystrixDashboardApp.java
@@ -2,16 +2,14 @@ package com.ming.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableHystrix
-@EnableHystrixDashboard
+@EnableHystrixDashboard	
 @SpringCloudApplication
-public class DalstonHystrixDashboardApplication {
+public class DalstonHystrixDashboardApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DalstonHystrixDashboardApplication.class, args);
+		SpringApplication.run(DalstonHystrixDashboardApp.class, args);
 	}
 
 }
